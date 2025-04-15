73d204428b0ce14ed1a72fe74d934bf55104efd3
kofiobuomagyare <kofiobuomagyare@gmail.com>
2025-04-15 20:10:41 +0000

    Add method to retrieve appointments by user ID in AppointmentRepo

--- src/main/java/com/nsaano/app/backend/Repo/AppointmentRepo.java
+++ src/main/java/com/nsaano/app/backend/Repo/AppointmentRepo.java
@@ -1,7 +1,11 @@
 package com.nsaano.app.backend.Repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.nsaano.app.backend.Models.Appointment;
 
 public interface AppointmentRepo extends JpaRepository<Appointment, Appointment.AppointmentId> {
+
+    List<Appointment> findByUserId(String userId);
 }
