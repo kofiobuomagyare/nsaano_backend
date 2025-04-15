3c8ea0158ec8743d6d502875c5544a423aaac138
kofiobuomagyare <kofiobuomagyare@gmail.com>
2025-04-15 21:36:02 +0000

    Format UserRepo interface methods for consistency

--- src/main/java/com/nsaano/app/backend/Repo/UserRepo.java
+++ src/main/java/com/nsaano/app/backend/Repo/UserRepo.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nsaano.app.backend.Models.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-User findByPhoneNumberOrEmail(String phoneNumber, String email);
-User findByEmail(String email);
-User findByPhoneNumber(String phoneNumber);
-User findByPhoneNumberAndPassword(String phoneNumber, String password);
+    User findByPhoneNumberOrEmail(String phoneNumber, String email); // This is fine as long as your entity has the same field names
+    User findByEmail(String email); 
+    User findByPhoneNumber(String phoneNumber);
+    User findByPhoneNumberAndPassword(String phoneNumber, String password);
+    
 }
